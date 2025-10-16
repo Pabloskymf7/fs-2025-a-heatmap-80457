2c4cce65a9eee9036b375830f3f02c7180b31ef2
Pablo <pmiguelferrer@gmail.com>
2025-10-16 10:12:46 +0100

    Agregar archivos de proyecto.

---
+++ fs-2025-a-heatmap-80457/Components/Data/HeatMapDataPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace fs_2025_a_heatmap_80457.Components.Data
+{
+    public class HeatMapDataPoint
+    {
+        public int Value { get; set; }
+        public string Label { get; set; } = string.Empty;
+    }
+}
